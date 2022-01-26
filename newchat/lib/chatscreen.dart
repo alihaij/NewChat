389329dc2b6da47c16e90a317971a1553251bba0
alihaij <a3haij@gmail.com>
2022-01-27 00:12:40 +0300

    Initial commit

---
+++ newchat/lib/chatscreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+var text;
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 7,
+        itemBuilder: (context, index) => Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('This works!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          firestore
+              .collection('chats/pnKqlUv26roBQQSTCaQt/massages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((document) {
+              print(
+                document['text'],
+              );
+            });
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ newchat/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'chatscreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
